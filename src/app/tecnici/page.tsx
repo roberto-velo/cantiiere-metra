b73231cc6452c6f192e2b0290416d1be7f66ab74
Roberto Velo <roberto.velo14@gmail.com>
2025-08-05 12:02:50 +0000

    nella pagina tecnici attiva il tasto nuovo tecnico

--- src/app/tecnici/page.tsx
+++ src/app/tecnici/page.tsx
@@ -27,7 +27,7 @@ export default function TecniciPage() {
                 </p>
             </div>
             <Button asChild>
-                <Link href="#">
+                <Link href="/tecnici/nuovo">
                 <PlusCircle className="mr-2 h-4 w-4" />
                 Nuovo Tecnico
                 </Link>
