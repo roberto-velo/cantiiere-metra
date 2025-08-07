d185cc88c422559b7f17ca151b9f5a4213fbe34c
Roberto Velo <roberto.velo14@gmail.com>
2025-08-07 10:53:09 +0000

    Error fetching all clients: {}

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type {NextConfig} from 'next';
+require('dotenv').config();
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -18,6 +19,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+   env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  }
 };
 
 export default nextConfig;
